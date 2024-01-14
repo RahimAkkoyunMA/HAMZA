2cb4745be92dd5dcad025380d80138d99fac2f14
Asalettin Soylu <asalettin.soylu@outlook.com>
2024-01-14 21:30:59 +0100

    Text in Justus geändert

--- Utku.java
+++ Utku.java
@@ -1,7 +1,7 @@
 public class Utku {
     public static void main(String[] args) {
         
-        System.out.println("Hallo ich bin Uktu!");
-        
+        System.out.println("Hallo ich bin Justus");
+
     }
 }
