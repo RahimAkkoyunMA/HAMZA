5c1fe678fe5a5e02ee7c041bc1991391191b5d99
Asalettin Soylu <asalettin.soylu@outlook.com>
2024-02-07 18:50:22 +0100

    Buch beispiel getestet

--- Kerem.java
+++ Kerem.java
@@ -1,5 +1,7 @@
 public class Kerem {
     public static void main(String[] args) {
-        System.out.println( "Hello Javanese" );
+        System.out.println( "This is a normal output" );
+        System.err.println( "This is an error output" );
+        
     }
-}     
+}      
